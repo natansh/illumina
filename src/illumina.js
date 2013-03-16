367efb012422e1041c797dea337ed003d99c5116
Natansh Verma <natansh.verma@gmail.com>
2013-03-16 16:28:24 +0530

    Cleaned up the illumina source file to start from scratch.

--- src/illumina.js
+++ src/illumina.js
@@ -6,33 +6,17 @@
  * Licensed under the MIT license.
  */
 
-(function($) {
+;(function($) {
 
+  var settings = 
   // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  $.fn.illumina = function(options) {
+
+    var settings = $.extend({
+    }, options);
+
+    return this.each(function() {
+      var $this = $(this);
     });
   };
-
-  // Static method.
-  $.awesome = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
-
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
-
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
-
 }(jQuery));
